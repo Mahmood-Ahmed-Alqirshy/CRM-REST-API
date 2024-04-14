87e2eb16dabe4ab8d4d4b1a9c1deb03f37ac27f8
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-04-14 03:22:02 +0300

    Add admin user as a seed

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -43,6 +44,12 @@ class User extends Authenticatable
         return [
             // 'email_verified_at' => 'datetime',
             'password' => 'hashed',
+        ];
+    }
+
+    public static function seeds() {
+        return [
+            ['username' => 'admin', 'password' => Hash::make('secret123')],
         ];
     }
 }
